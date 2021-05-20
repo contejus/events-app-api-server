14db7f8bb610dda8c4fee93987d71b55939725aa
Tejus <22385070+contejus@users.noreply.github.com>
2021-05-20 19:44:41 +0000

    add Dockerfiles and test changes

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Tejus team';
-    version = '1.0.1';
+    team = 'Tejus';
+    version = '1.0.2';
 }
 module.exports = AppConfig;
